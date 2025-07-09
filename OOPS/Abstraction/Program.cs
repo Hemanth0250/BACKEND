04e2b2f6e0c0041ad22aa2ed2e5042c19617f6c8
Hemanth0250 <anuhemu25@gmail.com>
2025-07-09 15:12:13 +0530

    abstraction

---
+++ OOPS/Abstraction/Program.cs
@@ -0,0 +1,41 @@
+﻿//Abstraction
+using System;
+
+namespace AbstractionExample
+{
+    // Abstract class
+    public abstract class Animal
+    {
+        // Abstract method has no body
+        // It must be implemented in derived classes
+        public abstract void MakeSound();
+
+        // Regular method 
+        public void Eat()
+        {
+            Console.WriteLine("Animal is eating...");
+        }
+    }
+
+    // Derived class
+    public class Dog : Animal
+    {
+        // Providing body to abstract method
+        public override void MakeSound()
+        {
+            Console.WriteLine("Dog says: Woof!");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            // Animal animal = new Animal(); // error, cannot create instance of abstract class
+            Dog dog = new Dog();
+            dog.MakeSound(); 
+            dog.Eat();      
+        }
+    }
+}
+
