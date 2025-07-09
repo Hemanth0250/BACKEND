5b1d2252a9084d58c0e929bbc778717c0a76dd65
Hemanth0250 <anuhemu25@gmail.com>
2025-07-09 14:14:23 +0530

    Inheritance

---
+++ OOPS/Inheritance/Program.cs
@@ -0,0 +1,46 @@
+﻿//inhertiance and sealed classes
+using System;
+
+namespace InheritanceExample
+{
+    // class vehicle inherited by car class
+    public class Vehicle
+    {
+        public string brand = "BMW";
+        public void Mode()
+        {
+            Console.WriteLine("RACING...");
+        }
+    }
+
+    // Derived class
+    public class Car : Vehicle
+    {
+        public string model = "M4";
+    }
+
+    // Sealed class 
+    public sealed class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Animal is eating");
+        }
+    }
+
+    
+    //public class Dog : Animal { } // Error,Animal is sealed and cannot be inherited
+    class Program
+    {
+        static void Main()
+        {
+            Car car = new Car();
+            car.Mode();  //inherited method from Vehicle
+            //inherited field from Vehicle
+            Console.WriteLine($"Brand: {car.brand}, Model: {car.model}");
+            //sealed class
+            Animal animal = new Animal();
+            animal.Eat();  
+        }
+    }
+}
