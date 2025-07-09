ce17c831997e7dc426dcf0e5a23bed3fa8390b95
Hemanth0250 <anuhemu25@gmail.com>
2025-07-09 14:49:49 +0530

    polymorphism

---
+++ OOPS/Polymorphism/Program.cs
@@ -0,0 +1,45 @@
+﻿//polymorphism example
+using System;
+namespace PolymorphismExample
+{
+    // Base class
+    public class Vehicle
+    {
+        //using virtual method to allow overriding
+        public virtual void Model()
+        {
+            Console.WriteLine("Cars are cars,thats it!");
+        }
+    }
+
+    // Derived class
+    public class BMW : Vehicle
+    {
+        public override void Model()
+        {
+            Console.WriteLine("BMW is a sport car");
+        }
+    }
+
+    // Another derived class
+    public class Mclearn : Vehicle
+    {
+        public override void Model()
+        {
+            Console.WriteLine("Mclearn is a super car");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Vehicle myBMW = new BMW();
+            Vehicle myMc = new Mclearn();
+
+            myBMW.Model();  // Output: BMW is a sport car
+            myMc.Model();   // Output: Mclearm is a super car
+        }
+    }
+}
