77b938755dbb19633e51c6d266600dffcbf99656
Hemanth0250 <anuhemu25@gmail.com>
2025-07-09 17:14:16 +0530

    interface

---
+++ OOPS/Interface/Program.cs
@@ -0,0 +1,45 @@
+﻿//interface
+using System;
+
+namespace InterfaceExample
+{
+    // Interface definition
+    public interface IAnimal
+    {
+        void MakeSound();          // Method (no body)
+        string Name { get; set; }  // Property
+    }
+
+    // Class implementing the interface
+    public class Dog : IAnimal
+    {
+        public string Name { get; set; }
+
+        public void MakeSound()
+        {
+            Console.WriteLine($"{Name} says: Woof!");
+        }
+    }
+
+    public class Cat : IAnimal
+    {
+        public string Name { get; set; }
+
+        public void MakeSound()
+        {
+            Console.WriteLine($"{Name} says: Meow!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IAnimal dog = new Dog { Name = "Buddy" };
+            dog.MakeSound();
+
+            IAnimal cat = new Cat { Name = "Whiskers" };
+            cat.MakeSound();
+        }
+    }
+}
